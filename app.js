02827b2c2ec063fe840402306d625d04a6da7629
FordangIT <93567754+FordangIT@users.noreply.github.com>
2022-06-23 21:10:57 +0900

    login.js

--- app.js
+++ app.js
@@ -5,7 +5,20 @@
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
+
+/* 연습 function onLoginBtnClick(){
+    const userName = loginInput.value;
+    if(userName === ""){
+        alert("Please Write your name"); 
+     } else if(userName.length > 15){
+            alert("please write your smalll name")
+        }
+    }
+
+loginButton.addEventListener("click", onLoginBtnClick); */
+
 function onLoginBtnClick(){
-    console.log("hello", loginInput.value); 
+    const userName = loginInput.value;
+    console.log(userName); 
 }
 loginButton.addEventListener("click", onLoginBtnClick); 
