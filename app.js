ee479a0a2ec3f799261920bb82dd1f7fa582ee69
FordangIT <93567754+FordangIT@users.noreply.github.com>
2022-07-05 12:04:31 +0900

    login.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 //const loginForm = document.getElementById("login-form"); 
 //const loginInput = loginForm.querySelector("input");
 ///const loginButton = loginForm.querySelector("button");
-
+/*
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
-
+*/
 
 /* 연습 function onLoginBtnClick(){
     const userName = loginInput.value;
@@ -17,8 +17,54 @@ const loginButton = document.querySelector("#login-form button");
 
 loginButton.addEventListener("click", onLoginBtnClick); */
 
+/*
 function onLoginBtnClick(){
     const userName = loginInput.value;
     console.log(userName); 
 }
 loginButton.addEventListener("click", onLoginBtnClick); 
+*/
+
+/*
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+
+function onLoginSubmit(){
+    const userName = loginInput.value;
+    console.log(userName);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+}
+loginForm.addEventListener("submit",onLoginSubmit);
+*/
+
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME =  "hidden"; 
+const USERNAME_KEY = "username"; 
+
+function onLoginBtnSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const userName = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, userName);
+   //greeting.innerText = "Hello " + userName;
+   paintGreetings(userName);
+}
+loginForm.addEventListener("submit", onLoginBtnSubmit)
+
+function paintGreetings(usernammme) {
+    greeting.innerText = `hello ${usernammme}`; 
+    greeting.classList.remove(HIDDEN_CLASSNAME); 
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginBtnSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
+
+console.log(savedUsername);
